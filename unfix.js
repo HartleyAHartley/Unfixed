4cb4fdced04f4bc0cee0143766658a6b9d34ef84
Alex Hartley <a.hartley@ufl.edu>
2019-01-22 21:20:37 -0500

    Active Recognizing

--- unfix.js
+++ unfix.js
@@ -1,10 +1,28 @@
+console.log("Loaded");
 var elems = document.body.getElementsByTagName("*");
 var len = elems.length
+var config = { attributes: true, childList: true, subtree: true };
+
+var callback = function(mutationsList, observer){
+    for(var mutation of mutationsList) {
+        if (mutation.attributeName == 'style'){
+           
+            for(var style of  mutation.target.style)
+                if(style == 'position'){
+                    console.log("hit");
+                    mutation.target.style.setProperty('position', 'absolute', 'important');
+                }
+        }
+    }
+}
 
 for (var i=0;i<len;i++) {
-
     if (window.getComputedStyle(elems[i],null).getPropertyValue('position') == 'fixed') {
-        elems[i].style.position = "absolute!important";
+        elems[i].style.setProperty('position', 'absolute', 'important');
+        console.log(elems[i]);
     }
 
 }
+
+var observer = new MutationObserver(callback);
+observer.observe(document.body, config);
